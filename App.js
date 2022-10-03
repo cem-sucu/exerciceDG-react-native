d9deaa8690d88c6beb7d83d0255d11b6a7419da4
cem-sucu <cems.cems.17@gmail.com>
2022-10-03 10:26:54 +0200

    initialisation exercice

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Exercice 1
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
 
 export default function App() {
+
+  const obj = [
+    {id: "1", name: "Stan", age: 45},
+    {id: "2", name: "Francine", age: 45},
+    {id: "3", name: "hayley", age: 18},
+    {id: "4", name: "Steve", age: 14},
+    {id: "5", name: "Roger", age: 1020},
+    {id: "6", name: "Klaus", age: 30},
+  ]
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        hello
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container:{
+    paddingTop: 50,
+    margin: 5 
+  }
 });
+
